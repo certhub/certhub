11b47ad7647f805d1c54303e5643b4db659d489a
Lorenz Schori <lo@znerol.ch>
2018-12-08 11:58:22 +0100

    try via CURL_ARGS

--- integration-test/test/test-dehydrated.sh
+++ integration-test/test/test-dehydrated.sh
@@ -6,18 +6,6 @@ set -x
 
 STATUS=1
 
-yum install -y nmap-ncat
-
-if env | grep PROXY; then
-    while ! nc 127.0.0.1 9050 < /dev/null ; do
-        sleep 1
-        continue
-    done
-    curl http://checkip.dyndns.org/
-    curl http://checkip.dyndns.org/
-    curl http://checkip.dyndns.org/
-fi
-
 su -s /bin/sh -c "/home/certhub/setup/csr-import.sh /home/certhub/setup/dehydrated-test" - certhub
 su -s /bin/sh -c "dehydrated --register --accept-terms --config /home/certhub/config/dehydrated-test.dehydrated" - certhub
 if systemctl start certhub-dehydrated-run@dehydrated-test.service; then
