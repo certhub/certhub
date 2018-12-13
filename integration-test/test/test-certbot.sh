d4de4ed1342bc71da431d7efc29033870f807bff
Lorenz Schori <lo@znerol.ch>
2018-12-13 22:58:48 +0100

    CSR is configuration not state, hence maintain it outside of repo

--- integration-test/test/test-certbot.sh
+++ integration-test/test/test-certbot.sh
@@ -6,16 +6,6 @@ set -x
 
 STATUS=1
 
-# Import csr from setup/certbot-test into certs.git
-cat <<EOF | /bin/su -s /bin/sh - certhub
-/usr/bin/env \
-    git gau-exec /home/certhub/certs.git \
-    git gau-ac \
-    git gau-xargs -I{} \
-    certhub-message-format "{}/certbot-test/csr.pem" req \
-    rsync -av "/home/certhub/setup/certbot-test" {}
-EOF
-
 if /bin/systemctl start certhub-certbot-run@certbot-test.service; then
     STATUS=0
 fi
--- integration-test/test/test-dehydrated.sh
+++ integration-test/test/test-dehydrated.sh
@@ -6,16 +6,6 @@ set -x
 
 STATUS=1
 
-# Import csr from setup/dehydrated-test into certs.git
-cat <<EOF | /bin/su -s /bin/sh - certhub
-/usr/bin/env \
-    git gau-exec /home/certhub/certs.git \
-    git gau-ac \
-    git gau-xargs -I{} \
-    certhub-message-format "{}/dehydrated-test/csr.pem" req \
-    rsync -av "/home/certhub/setup/dehydrated-test" {}
-EOF
-
 su -s /bin/sh -c "dehydrated --register --accept-terms --config /home/certhub/config/dehydrated-test.dehydrated" - certhub
 if systemctl start certhub-dehydrated-run@dehydrated-test.service; then
     STATUS=0
