a541a39876658054e0cbec55a25ec6e372c9cc4b
znerol <lo+github@znerol.ch>
2024-03-30 17:40:30 +0100

    chore(docs): Correct year and rtd theme (#78)

--- doc/conf.py
+++ doc/conf.py
@@ -17,12 +17,13 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 import os
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'Certhub'
-copyright = '2018, Lorenz Schori'
+copyright = '2024, Lorenz Schori'
 author = 'Lorenz Schori'
 
 # The short X.Y version
@@ -61,7 +62,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -73,15 +74,8 @@ pygments_style = None
 
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
